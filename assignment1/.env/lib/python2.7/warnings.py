52f78d531d5c0610d399636750bae00888bfa748
freefrog1986 <baihu1111@126.com>
2017-09-29 11:28:56 +0800

    asign1

---
+++ assignment1/.env/lib/python2.7/warnings.py
@@ -0,0 +1,1 @@
+/Users/freefrog/anaconda2/lib/python2.7/warnings.py
---
+++ assignment1/q2_gradcheck.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import numpy as np
+import random
+
+
+# First implement a gradient checker by filling in the following functions
+def gradcheck_naive(f, x):
+    """ Gradient check for a function f.
+
+    Arguments:
+    f -- a function that takes a single argument and outputs the
+         cost and its gradients
+    x -- the point (numpy array) to check the gradient at
+    """
+
+    rndstate = random.getstate()
+    random.setstate(rndstate)
+    fx, grad = f(x) # Evaluate function value at original point
+    h = 1e-4        # Do not change this!
+
+    # Iterate over all indexes in x
+    # flags=['multi_index'] can get all dimension index 
+    # Set op_flags to make array readable and writable.
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        ix = it.multi_index
+
+        # Try modifying x[ix] with h defined above to compute
+        # numerical gradients. Make sure you call random.setstate(rndstate)
+        # before calling f(x) each time. This will make it possible
+        # to test cost functions with built in randomness later.
+
+        random.setstate(rndstate)
+        tmp1 = np.copy(x) 
+        tmp1[ix] = tmp1[ix] + h
+        f1, _ = f(tmp1)
+        
+        random.setstate(rndstate)
+        tmp2 = np.copy(x) 
+        tmp2[ix] = tmp2[ix] - h
+        f2, _ = f(tmp2)
+        numgrad = (f1 - f2) / (2 * h)
+
+        print(numgrad,grad[ix])
+        print(max(1,abs(numgrad), abs(grad[ix])))
+
+        # Compare gradients
+        reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
+        if reldiff > 1e-5:
+            print "Gradient check failed."
+            print "First gradient error found at index %s" % str(ix)
+            print "Your gradient: %f \t Numerical gradient: %f" % (
+                grad[ix], numgrad)
+            return
+
+        it.iternext() # Step to next dimension
+
+    print "Gradient check passed!"
+
+
+def sanity_check():
+    """
+    Some basic sanity checks.
+    """
+    quad = lambda x: (np.sum(x ** 2), x * 2)
+
+    print "Running sanity checks..."
+    gradcheck_naive(quad, np.array(123.456))      # scalar test
+    gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
+    gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
+    print ""
+
+
+def your_sanity_checks():
+    """
+    Use this space add any additional sanity checks by running:
+        python q2_gradcheck.py
+    This function will not be called by the autograder, nor will
+    your additional tests be graded.
+    """
+    print "Running your sanity checks..."
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+
+
+if __name__ == "__main__":
+    sanity_check()
+    #your_sanity_checks()
